23a1fb6d6f02302d5838f98600f567b1f05a7e43
Matt Botz and Stephen Emery <botzmatt@yahoo.com>
2016-06-13 19:45:59 -0700

    Create array in JS to rearrange answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    
+  $("form").submit(function(event) {
+
+    var answers = [$("#question1").val(), $("#question2").val(), $("#question3").val(), $("#question4").val(), $("#question5").val()];
+    var cloneArray = answers.slice(0,2);
+    cloneArray = cloneArray.reverse();
+    cloneArray = cloneArray.concat(answers.slice(2));
+    console.log(cloneArray);
+    removed = cloneArray.splice(3, 2);
+
+
+    console.log(cloneArray);
+    console.log(removed);
+    event.preventDefault();
+  });
 });
